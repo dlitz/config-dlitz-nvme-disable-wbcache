05cc4817848002fa557c39747b2640fbf6b456fe
Darsey Litzenberger <dlitz@dlitz.net>
2024-10-08 15:16:30 -0600

    Clean up boilerplate cruft; update copyright

--- debian/config-dlitz-nvme-disable-wbcache.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the config-dlitz-nvme-disable-wbcache package.
-#
-0 4	* * *	root	[ -x /usr/bin/config-dlitz-nvme-disable-wbcache_maintenance ] && /usr/bin/config-dlitz-nvme-disable-wbcache_maintenance
